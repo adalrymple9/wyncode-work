b535799d219131ce6f8505bc08e87ccdf00d83ad
Andrea Dalrymple <andrea.dalrymple@yahoo.com>
2016-10-27 19:03:10 -0400

    adding my homework to git hub

---
+++ homework.rb
@@ -0,0 +1,9 @@
+height = ARGV[0].to_i
+output = ""
+height.times do |i|
+  output << " " * (((height * 2) - ((i + 1) * 2)) / 2)
+  output << "*" * ((i + 1) * 2)
+  output << "\n"
+end
+
+puts output
